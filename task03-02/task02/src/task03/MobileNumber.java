de5187725f88afed5e513a63b31a6248bb2d3227
Nikolay Tkachenko <110029945+Nik0layTkachenko@users.noreply.github.com>
2023-04-10 01:09:52 +0300

    Add files via upload

---
+++ task03-02/task02/src/task03/MobileNumber.java
@@ -0,0 +1,19 @@
+package task02;
+
+
+
+public class MobileNumber {
+    private String number;
+
+    public MobileNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPrefix() {
+        return number.substring(0, 3);
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
